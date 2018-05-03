c4c73255100f96c17703edbb0ce2ade7dda78ae2
Rachel Norris <rmnorris@well.ox.ac.uk>
2018-05-03 11:39:49 +0100

    add small tests

---
+++ test_make_prg.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import pytest
+from make_prg_from_msa import AlignedSeq
+
+
+def test_answers():
+    aseq = AlignedSeq("test/match.fa")
+    assert aseq.prg == "ACGTGTTTTGTAACTGTGCCACACTCTCGAGACTGCATATGTGTC"
+
+    aseq = AlignedSeq("test/nonmatch.fa")
+    assert aseq.prg == " 5 AAACGTGGTT 6 CCCCCCCCCC 5 "
+
+    aseq = AlignedSeq("test/match.nonmatch.fa")
+    assert aseq.prg == "AAACG 5 TGGTT 6 CCCCC 5 "
+
+    aseq = AlignedSeq("test/nonmatch.match.fa")
+    assert aseq.prg == " 5 AAACGT 6 CCCCCC 5 GGTT"
+
+    aseq = AlignedSeq("test/match.nonmatch.match.fa")
+    assert aseq.prg == "AAACG 5 T 6 C 5 GGTT"
+
+    aseq = AlignedSeq("test/shortmatch.nonmatch.match.fa")
+    assert aseq.prg == " 5 AAACGT 6 ATTTTC 5 GGTT"
+
+    aseq = AlignedSeq("test/match.nonmatch.shortmatch.fa")
+    assert aseq.prg == "AAAC 5 GTGGTT 6 CCCCCT 5 "
+
+    aseq = AlignedSeq("test/match.staggereddash.fa")
+    assert aseq.prg == "AAACGTGGTT"
