bce35b0f87b9983b300ca8839dabed4f9cb6bea7
Michael Hall <michael@mbh.sh>
2019-10-26 06:55:05 +0100

    reformat with black

--- make_prg/utils.py
+++ make_prg/utils.py
@@ -2,11 +2,14 @@ import logging
 from typing import Generator, Sequence
 import re
 
+
 def contains_only(seq, aset):
     """ Check whether sequence seq contains ONLY items in aset. """
     for c in seq:
-        if c not in aset: return False
+        if c not in aset:
+            return False
     return True
+
 
 def remove_duplicates(seqs: Sequence) -> Generator:
     seen = set()
@@ -20,23 +23,31 @@ def remove_duplicates(seqs: Sequence) -> Generator:
 def remove_gaps(sequence: str) -> str:
     return sequence.replace("-", "")
 
-#************/
+
+# ************/
 # GFA code */
-#***********/
-class GFA_Output():
+# ***********/
+class GFA_Output:
     """
     A simple class for converting a PRG string into a GFA file
     TODO: Update to GFA2 format
     """
-    def __init__(self, gfa_string = "", gfa_id = 0, gfa_site = 5):
+
+    def __init__(self, gfa_string="", gfa_id=0, gfa_site=5):
         self.gfa_string = gfa_string
         self.gfa_id = gfa_id
-        self.gfa_site = gfa_site;
-        self.delim_char = " " #This mirrors the AlignedSeq class.
+        self.gfa_site = gfa_site
+        self.delim_char = " "  # This mirrors the AlignedSeq class.
 
     def split_on_site(self, prg_string, site_num):
-        site_coords = [(a.start(), a.end()) for a in
-                       list(re.finditer('%s%d%s' % (self.delim_char, site_num, self.delim_char), prg_string))]
+        site_coords = [
+            (a.start(), a.end())
+            for a in list(
+                re.finditer(
+                    "%s%d%s" % (self.delim_char, site_num, self.delim_char), prg_string
+                )
+            )
+        ]
         last_pos = None
         split_strings = []
         for (start, end) in site_coords:
@@ -45,10 +56,11 @@ class GFA_Output():
         split_strings.append(prg_string[last_pos:])
         delim = "%s%d%s" % (self.delim_char, site_num, self.delim_char)
         check_string = delim.join(split_strings)
-        assert check_string == prg_string, "Something has gone wrong with the string split for site %d\nsplit_" \
-                                           "strings: %s" % (site_num, split_strings)
+        assert check_string == prg_string, (
+            "Something has gone wrong with the string split for site %d\nsplit_"
+            "strings: %s" % (site_num, split_strings)
+        )
         return split_strings
-
 
     def build_gfa_string(self, prg_string, pre_var_id=None):
         """Takes prg_string and builds a gfa_string with fragments
@@ -60,10 +72,13 @@ class GFA_Output():
             prgs = self.split_on_site(prg_string, self.gfa_site)
             logging.debug("prgs: %s", prgs)
             assert len(prgs) == 3, "Invalid prg sequence %s for site %d and id %d" % (
-                prg_string, self.gfa_site, self.gfa_id)
+                prg_string,
+                self.gfa_site,
+                self.gfa_id,
+            )
 
             # add pre-var site string and links from previous seq fragments
-            if prgs[0] != '':
+            if prgs[0] != "":
                 self.gfa_string += "S\t%d\t%s\tRC:i:0\n" % (self.gfa_id, prgs[0])
             else:
                 # adds an empty node for empty pre var site seqs
@@ -78,21 +93,29 @@ class GFA_Output():
             # this site, saving the end id for each haplotype
             vars = self.split_on_site(prgs[1], self.gfa_site + 1)
             assert len(vars) > 1, "Invalid prg sequence %s for site %d and id %d" % (
-                prg_string, self.gfa_site + 1, self.gfa_id)
+                prg_string,
+                self.gfa_site + 1,
+                self.gfa_id,
+            )
             logging.debug("vars: %s", vars)
             self.gfa_site += 2
             logging.debug("gfa_site: %d", self.gfa_site)
             for var_string in vars:
                 if pre_var_id != None:
-                    self.gfa_string += "L\t%d\t+\t%d\t+\t0M\n" % (pre_var_id, self.gfa_id)
-                var_end_ids = self.build_gfa_string(prg_string=var_string, pre_var_id=pre_var_id)
+                    self.gfa_string += "L\t%d\t+\t%d\t+\t0M\n" % (
+                        pre_var_id,
+                        self.gfa_id,
+                    )
+                var_end_ids = self.build_gfa_string(
+                    prg_string=var_string, pre_var_id=pre_var_id
+                )
                 end_ids.extend(var_end_ids)
 
             prg_string = prgs[2]
             pre_var_id = None
 
         # finally add the final bit of sequence after variant site
-        if prg_string != '':
+        if prg_string != "":
             self.gfa_string += "S\t%d\t%s\tRC:i:0\n" % (self.gfa_id, prg_string)
         else:
             self.gfa_string += "S\t%d\t%s\tRC:i:0\n" % (self.gfa_id, "*")
@@ -103,11 +126,12 @@ class GFA_Output():
         self.gfa_id += 1
         return return_id
 
+
 def write_gfa(outfile, prg_string):
     """
     Writes a gfa file from prg string.
     """
-    with open(outfile, 'w') as f:
+    with open(outfile, "w") as f:
         # initialize gfa_string, id and site, then update string with the prg
         gfa_string = "H\tVN:Z:1.0\tbn:Z:--linear --singlearr\n"
         gfa_id = 0
@@ -116,9 +140,11 @@ def write_gfa(outfile, prg_string):
         gfa_obj.build_gfa_string(prg_string=prg_string)
         f.write(gfa_obj.gfa_string)
 
-#******************/
+
+# ******************/
 # Write PRG code */
-#*****************/
+# *****************/
+
 
 class Integer_Encoder:
     """
@@ -127,11 +153,9 @@ class Integer_Encoder:
         Production: run `run()`
         Serialisation: run `write()`
     """
-    DNA = {"A" : 1,
-           "C" : 2,
-           "G" : 3,
-           "T" : 4}
-    NUM_BYTES = 4 # How many bytes to use per serialised integer?
+
+    DNA = {"A": 1, "C": 2, "G": 3, "T": 4}
+    NUM_BYTES = 4  # How many bytes to use per serialised integer?
 
     def __init__(self, prg_string):
         self.marker_units = prg_string.split()
@@ -142,20 +166,23 @@ class Integer_Encoder:
             self.vector.extend(self._encode_unit(unit))
 
     def write(self, fpath):
-        with open(fpath, 'wb') as f:
+        with open(fpath, "wb") as f:
             for integer in self.vector:
-                f.write(integer.to_bytes(self.NUM_BYTES, "little")) #Little endian
+                f.write(integer.to_bytes(self.NUM_BYTES, "little"))  # Little endian
 
     def _DNA_to_int(self, input_char):
-        assert input_char in self.DNA, logging.error(f"Conversion error: char {input_char} is not in {self.DNA}")
+        assert input_char in self.DNA, logging.error(
+            f"Conversion error: char {input_char} is not in {self.DNA}"
+        )
         return self.DNA[input_char]
 
-    def _encode_unit(self,input_string):
+    def _encode_unit(self, input_string):
         if input_string[0] in self.DNA:
             output = list(map(self._DNA_to_int, input_string))
         else:
             output = [int(input_string)]
         return output
+
 
 def write_prg(outf_prefix, prg_string):
     """
@@ -163,13 +190,11 @@ def write_prg(outf_prefix, prg_string):
     Writes it as a human readable string, and also as an integer vector
     """
     out_name = f"{outf_prefix}.prg"
-    with open(out_name, 'w') as f:
+    with open(out_name, "w") as f:
         f.write(prg_string)
 
-    out_name = f'{outf_prefix}.bin'
+    out_name = f"{outf_prefix}.bin"
     int_enc = Integer_Encoder(prg_string)
     int_enc.run()
     # print(int_enc.vector.elements)
     int_enc.write(out_name)
-
-
