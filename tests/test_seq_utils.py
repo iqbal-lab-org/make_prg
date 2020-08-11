9793a1af36b04e742257e60dca401b3415830be4
Brice Letcher <bletcher@ebi.ac.uk>
2020-08-11 18:38:24 +0100

    Alignment column of all '-' no longer a match

--- tests/test_seq_utils.py
+++ tests/test_seq_utils.py
@@ -1,12 +1,10 @@
 import unittest
 
-from hypothesis import given
-from hypothesis.strategies import text
 from Bio import AlignIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
-from make_prg.utils import remove_gaps, get_interval_seqs
+from make_prg.utils import get_interval_seqs
 
 
 class TestGetIntervals(unittest.TestCase):
@@ -29,43 +27,3 @@ class TestGetIntervals(unittest.TestCase):
         result = get_interval_seqs(alignment)
         expected = ["TTTT", "AAAA", "CCC"]
         self.assertEqual(expected, result)
-
-
-class TestRemoveGaps(unittest.TestCase):
-    def test_empty_string_returns_empty(self):
-        seq = ""
-
-        actual = remove_gaps(seq)
-        expected = ""
-
-        self.assertEqual(actual, expected)
-
-    def test_string_with_no_gaps_returns_original(self):
-        seq = "ACGT"
-
-        actual = remove_gaps(seq)
-        expected = seq
-
-        self.assertEqual(actual, expected)
-
-    def test_string_with_one_gaps_returns_original_without_gap(self):
-        seq = "ACGT-"
-
-        actual = remove_gaps(seq)
-        expected = "ACGT"
-
-        self.assertEqual(actual, expected)
-
-    def test_string_with_many_gaps_returns_original_without_gap(self):
-        seq = "A-CGT--"
-
-        actual = remove_gaps(seq)
-        expected = "ACGT"
-
-        self.assertEqual(actual, expected)
-
-    @given(text())
-    def test_all_input_space_doesnt_break(self, seq):
-        actual = remove_gaps(seq)
-
-        self.assertFalse("-" in actual)
