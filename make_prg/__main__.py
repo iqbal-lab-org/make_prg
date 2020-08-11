5137d1e3bb2cfd4e66a5ff9cf89644322a64cdc8
Brice Letcher <bletcher@ebi.ac.uk>
2020-08-11 14:15:27 +0100

    Defer subcommand argument parsing to subcommands dir

--- make_prg/__main__.py
+++ make_prg/__main__.py
@@ -1,9 +1,7 @@
 import argparse
 
-import make_prg
-
-NESTING_LVL = 5
-MIN_MATCH_LEN = 7
+from make_prg import __version__
+from make_prg.subcommands import prg_from_msa
 
 
 def main():
@@ -13,77 +11,12 @@ def main():
         description="script to run make_prg subcommands",
     )
 
-    parser.add_argument("--version", action="version", version=make_prg.__version__)
+    parser.add_argument("--version", action="version", version=__version__)
     subparsers = parser.add_subparsers(
         title="Available subcommands", help="", metavar=""
     )
 
-    # _____________________________ prg_from_msa ______________________________#
-    subparser_prg_from_msa = subparsers.add_parser(
-        "prg_from_msa",
-        usage="make_prg prg_from_msa [options] <MSA input file>",
-        help="Make PRG from multiple sequence alignment",
-    )
-
-    subparser_prg_from_msa.add_argument(
-        "MSA",
-        action="store",
-        type=str,
-        help=(
-            "Input file: a multiple sequence alignment in supported alignment_format. "
-            "If not in aligned fasta alignment_format, use -f to input the "
-            "alignment_format type"
-        ),
-    )
-    subparser_prg_from_msa.add_argument(
-        "-f",
-        "--alignment_format",
-        dest="alignment_format",
-        action="store",
-        default="fasta",
-        help=(
-            "Alignment format of MSA, must be a biopython AlignIO input "
-            "alignment_format. See http://biopython.org/wiki/AlignIO. Default: fasta"
-        ),
-    )
-    subparser_prg_from_msa.add_argument(
-        "--max_nesting",
-        dest="max_nesting",
-        action="store",
-        type=int,
-        default=NESTING_LVL,
-        help="Maximum number of levels to use for nesting. Default: {}".format(
-            NESTING_LVL
-        ),
-    )
-    subparser_prg_from_msa.add_argument(
-        "--min_match_length",
-        dest="min_match_length",
-        action="store",
-        type=int,
-        default=MIN_MATCH_LEN,
-        help=(
-            "Minimum number of consecutive characters which must be identical for a "
-            "match. Default: {}".format(MIN_MATCH_LEN)
-        ),
-    )
-    subparser_prg_from_msa.add_argument(
-        "-p", "--prefix", dest="output_prefix", action="store", help="Output prefix"
-    )
-    subparser_prg_from_msa.add_argument(
-        "--no_overwrite",
-        dest="no_overwrite",
-        action="store_true",
-        help="Do not overwrite pre-existing prg file with same name",
-    )
-    subparser_prg_from_msa.add_argument(
-        "-v",
-        "--verbose",
-        dest="verbose",
-        action="store_true",
-        help="Run with high verbosity " "(debug level logging)",
-    )
-    subparser_prg_from_msa.set_defaults(func=make_prg.subcommands.prg_from_msa.run)
+    prg_from_msa.register_parser(subparsers)
 
     args = parser.parse_args()
 
