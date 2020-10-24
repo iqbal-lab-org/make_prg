8d9329e1958f84be1d3b49df845c401dac25bba2
Brice Letcher <bletcher@ebi.ac.uk>
2020-10-24 18:17:44 +0100

    One reference based clustering (1)
    * Add thresholds
    * Add worker functions
    * Add function deciding whether a set of sequences is 'one-ref like'
      using worker functions
    * Tests of all added functions

--- make_prg/from_msa/cluster_sequences.py
+++ make_prg/from_msa/cluster_sequences.py
@@ -1,6 +1,8 @@
 import logging
 from collections import defaultdict
-from typing import List, Dict
+from typing import List, Dict, Iterator
+from itertools import starmap, repeat
+from collections import Counter
 
 import numpy as np
 from sklearn.cluster import KMeans
@@ -12,6 +14,9 @@ Clustering = List[IDs]
 Sequence = str
 Sequences = List[str]
 KmerIDs = Dict[str, int]
+
+DISTANCE_THRESHOLD: float = 0.2
+LENGTH_THRESHOLD: int = 5
 
 
 def count_distinct_kmers(seqs: Sequences, kmer_size: int) -> KmerIDs:
@@ -47,6 +52,52 @@ def count_kmer_occurrences(seqs: Sequences, kmers: KmerIDs):
     return result
 
 
+def get_majority_char_in_column(sequences: Sequences, col_idx: int) -> str:
+    max_idx = len(sequences[0]) - 1
+    if not (0 <= col_idx <= max_idx):
+        raise ValueError(f"Column index {col_idx} not in range(0,{max_idx})")
+
+    column = [seq[col_idx] for seq in sequences]
+    return Counter(column).most_common(1)[0][0]
+
+
+def get_majority_string(sequences: Sequences) -> str:
+    seqlen = len(sequences[0])
+    if not all([len(seq) == seqlen for seq in sequences]):
+        raise ValueError("Not all sequences have the same length")
+
+    arguments = zip(repeat(sequences), range(seqlen))
+    return "".join(starmap(get_majority_char_in_column, arguments))
+
+
+def hamming_distance(seq1: Sequence, seq2: Sequence) -> int:
+    seqlen = len(seq1)
+    result = 0
+    for i in range(seqlen):
+        if seq1[i] != seq2[i]:
+            result += 1
+    return result
+
+
+def get_distances(sequences: Sequences, consensus_string: Sequence) -> Iterator[int]:
+    arguments = zip(sequences, repeat(consensus_string))
+    return starmap(hamming_distance, arguments)
+
+
+def get_one_ref_like_threshold_distance(seqlen: int) -> int:
+    if seqlen < LENGTH_THRESHOLD:
+        return 1
+    else:
+        return int(DISTANCE_THRESHOLD * seqlen)
+
+
+def sequences_are_one_reference_like(sequences: Sequences) -> bool:
+    majority_string = get_majority_string(sequences)
+    distance_iterator = get_distances(sequences, majority_string)
+    min_thresh = get_one_ref_like_threshold_distance(len(sequences[0]))
+    return all(map(lambda dist: dist <= min_thresh, distance_iterator))
+
+
 def kmeans_cluster_seqs_in_interval(
     interval: List[int], alignment: MSA, kmer_size: int,
 ) -> Clustering:
@@ -67,7 +118,7 @@ def kmeans_cluster_seqs_in_interval(
             small_seq_to_ids[seq].append(record.id)
 
     clustered_ids = []
-    if len(seq_to_ids) < 2:
+    if len(seq_to_ids) <= 2:
         clustered_ids.extend(seq_to_ids.values())
 
     else:
--- tests/from_msa/test_cluster_sequences.py
+++ tests/from_msa/test_cluster_sequences.py
@@ -5,11 +5,20 @@ import random
 from numpy import array, array_equal
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+from pandas import DataFrame
 
 from make_prg.seq_utils import standard_bases
 from make_prg.from_msa.cluster_sequences import (
     count_distinct_kmers,
     count_kmer_occurrences,
+    get_majority_char_in_column,
+    get_majority_string,
+    hamming_distance,
+    get_distances,
+    DISTANCE_THRESHOLD,
+    LENGTH_THRESHOLD,
+    get_one_ref_like_threshold_distance,
+    sequences_are_one_reference_like,
     kmeans_cluster_seqs_in_interval,
 )
 
@@ -51,18 +60,148 @@ class TestCountMatrix(TestCase):
         assert array_equal(result, expected)
 
 
+class TestOneRefWorkerFunctions(TestCase):
+    sequences = [
+        "CATATAAAATA",
+        "CATATAA-ATA",
+        "GGGGCGGGCCC",
+        "GGGGCGGGCGC",
+        "GGGGCCGGCCC",
+        "GGGGCGGGCCC",
+    ]
+
+    def test_majority_char_bad_index_fail(self):
+        with self.assertRaises(ValueError):
+            get_majority_char_in_column(self.sequences, len(self.sequences[0]))
+
+    def test_majority_char_returns_majority_char(self):
+        actual = get_majority_char_in_column(self.sequences, 0)
+        self.assertEqual(actual, "G")
+
+    def test_majority_char_returns_majority_nonbase_char(self):
+        sequences = ["AAA-", "AAT-", "AAAA"]
+        actual = get_majority_char_in_column(sequences, 3)
+        self.assertEqual(actual, "-")
+
+    def test_majority_string_bad_lengths_fails(self):
+        sequences = ["AAA-", "AT-", "AAAA"]
+        with self.assertRaises(ValueError):
+            get_majority_string(sequences)
+
+    def test_majority_string_correct(self):
+        actual = get_majority_string(self.sequences)
+        expected = "GGGGCGGGCCC"
+        self.assertEqual(actual, expected)
+
+    def test_hamming_distance_same_sequences(self):
+        actual = hamming_distance(self.sequences[0], self.sequences[0])
+        self.assertEqual(actual, 0)
+
+    def test_hamming_distance_different_sequences(self):
+        actual = hamming_distance(self.sequences[0], self.sequences[1])
+        self.assertEqual(actual, 1)
+        actual = hamming_distance(self.sequences[3], self.sequences[4])
+        self.assertEqual(actual, 2)
+
+    def test_get_all_distances(self):
+        actual = list(get_distances(self.sequences, self.sequences[0]))
+        expected = [0, 1, 11, 11, 11, 11]
+        self.assertEqual(actual, expected)
+
+    def test_get_all_distances_to_majority_string(self):
+        majority_string = get_majority_string(self.sequences)
+        actual = list(get_distances(self.sequences, majority_string))
+        expected = [11, 11, 0, 1, 1, 0]
+        self.assertEqual(actual, expected)
+
+
+class TestOneRefLikeClusters(TestCase):
+    """
+    Disclaimer:
+    A heuristic is used to determine whether a set of sequences
+    is 'one-ref like' based on a length threshold and a distance threshold.
+
+    If either of those parameters is changed, below tests can start to fail;
+    I placed in assertions to point to why.
+    """
+
+    def test_GivenSequencesWithDifferentLengths_Fails(self):
+        sequences = ["AT", "AA", "CCC"]
+        with self.assertRaises(ValueError):
+            sequences_are_one_reference_like(sequences)
+
+    def test_GivenSnpsOnly_AreRefLike(self):
+        sequences = ["A", "T", "C"]
+        self.assertTrue(sequences_are_one_reference_like(sequences))
+
+    def test_GivenShortSeqsWithMoreThanOneDiff_AreNotRefLike(self):
+        sequences = ["AA", "TT", "CC"]
+        seqlen = len(sequences[0])
+        self.assertTrue(seqlen < LENGTH_THRESHOLD)
+        self.assertEqual(get_one_ref_like_threshold_distance(seqlen), 1)
+        self.assertFalse(sequences_are_one_reference_like(sequences))
+
+    def test_GivenLongSequencesWithDiffBelowThreshold_AreRefLike(self):
+        sequences = ["AATTA", "AATTT", "TATTA"]
+        # Sequences are above length threshold
+        seqlen = len(sequences[0])
+        self.assertTrue(seqlen >= LENGTH_THRESHOLD)
+        # We tolerate up to one diff against majority string
+        self.assertEqual(get_one_ref_like_threshold_distance(seqlen), 1)
+
+        # Here's the majority string
+        self.assertEqual(get_majority_string(sequences), "AATTA")
+
+        # No sequence is > distance threshold from the majority string
+        self.assertTrue(sequences_are_one_reference_like(sequences))
+
+    def test_GivenLongSequencesWithDiffAboveThreshold_AreNotRefLike(self):
+        sequences = ["AATTA", "AATTT", "TATTA", "GCGGG"]
+        seqlen = len(sequences[0])
+        self.assertTrue(seqlen >= LENGTH_THRESHOLD)
+        self.assertEqual(get_one_ref_like_threshold_distance(seqlen), 1)
+        self.assertEqual(get_majority_string(sequences), "AATTA")
+        self.assertFalse(sequences_are_one_reference_like(sequences))
+
+    def test_GivenLongSequencesFromRealDataWithDiffBelowThreshold_AreRefLike(self):
+        """This data appeared in issue #15 and used to get clustered, producing ambiguous graphs with paths spelling the same sequence"""
+        sequences = [
+            "gctccgccggtcccgccggtcc",
+            "gctccgccgggcccgccggtcc",
+            "tctccgccggtcccgccggtcc",
+            "gctcagccggtcccgccggtcc",
+            "gctccgccggtcccaccggtcc",
+            "gctccgccggtaccgccggtcc",
+            "gctccgctggtcccgccggtcc",
+            "gctccgccggtcccgctggtcc",
+            "gctccgccggtcccgccggtct",
+            "gctccgccggtcccgcctgtcc",
+            "gctccgccggtcctgccggtcc",
+        ]
+        seqlen = len(sequences[0])
+        self.assertEqual(get_one_ref_like_threshold_distance(seqlen), 4)
+        self.assertTrue(sequences_are_one_reference_like(sequences))
+
+
 class TestClustering_Trivial(TestCase):
     def test_one_seq_returns_single_id(self):
         alignment = MSA([SeqRecord(Seq("AAAT"), id="s1")])
         result = kmeans_cluster_seqs_in_interval([0, 3], alignment, 1)
         self.assertEqual(result, [["s1"]])
 
-    def test_two_identical_seqs_returns_two_ids_clustered(self):
-        alignment = MSA(
-            [SeqRecord(Seq("AAAT"), id="s1"), SeqRecord(Seq("AAAT"), id="s2"),]
-        )
+    @patch("make_prg.from_msa.cluster_sequences.KMeans")
+    def test_GivenTwoIdenticalSeqs_NoKmeansAndOneCluster(self, mockKMeans):
+        alignment = make_alignment(["AAAT", "AAAT"])
         result = kmeans_cluster_seqs_in_interval([0, 3], alignment, 1)
-        self.assertEqual(result, [["s1", "s2"]])
+        mockKMeans.assert_not_called()
+        self.assertEqual(result, [["s0", "s1"]])
+
+    @patch("make_prg.from_msa.cluster_sequences.KMeans")
+    def test_GivenTwoDifferentSeqs_NoKmeansAndTwoClusters(self, mockKMeans):
+        alignment = make_alignment(["AAAT", "ATAT"])
+        result = kmeans_cluster_seqs_in_interval([0, 3], alignment, 1)
+        mockKMeans.assert_not_called()
+        self.assertEqual(result, [["s0"], ["s1"]])
 
 
 class TestClustering_SmallSequences(TestCase):
@@ -100,12 +239,10 @@ class TestClustering_SmallSequences(TestCase):
         self.assertEqual([["s1", "s4"], ["s2"], ["s3"]], result)
 
 
-class TestClustering_Kmeans(TestCase):
+class TestClustering_RunKmeans(TestCase):
     @patch("make_prg.from_msa.cluster_sequences.KMeans.fit")
-    def test_GivenTwoSequencesAboveKmerSize_KMeansClusteringCalled(self, mockfit):
-        alignment = MSA(
-            [SeqRecord(Seq("AAAT"), id="s1"), SeqRecord(Seq("TTTT"), id="s2")]
-        )
+    def test_GivenThreeSequencesAboveKmerSize_KMeansClusteringCalled(self, mockfit):
+        alignment = make_alignment(["AAAT", "TTTT", "ATAT"])
         mockfit.return_value = Mock(inertia_=0)
         result = kmeans_cluster_seqs_in_interval([0, 3], alignment, 2)
         mockfit.assert_called_once()
