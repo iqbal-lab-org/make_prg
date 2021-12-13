269569b55574ec88e79e177f055c9f7da9f3acbb
leoisl <leandro.ishi.lima@gmail.com>
2021-12-13 19:32:41 +0000

    Removing useless conditional
    Co-authored-by: Michael Hall <michael@mbh.sh>

--- make_prg/utils/io_utils.py
+++ make_prg/utils/io_utils.py
@@ -42,11 +42,10 @@ def remove_empty_folders(path: str, remove_root: bool = True):
 
     # remove empty subfolders
     files = os.listdir(path)
-    if len(files):
-        for f in files:
-            fullpath = os.path.join(path, f)
-            if os.path.isdir(fullpath):
-                remove_empty_folders(fullpath)
+    for f in files:
+        fullpath = os.path.join(path, f)
+        if os.path.isdir(fullpath):
+            remove_empty_folders(fullpath)
 
     # if folder empty, delete it
     files = os.listdir(path)
