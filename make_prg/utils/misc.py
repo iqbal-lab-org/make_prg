6d7c6524d8f085538ba6db721c305eea388734d8
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-12-13 22:52:32 +0000

    Refactoring typing in misc.py

--- make_prg/utils/misc.py
+++ make_prg/utils/misc.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import List, Any
 from itertools import groupby, chain
 
 
-def remove_duplicated_consecutive_elems_from_list(the_list: List) -> List:
+def remove_duplicated_consecutive_elems_from_list(the_list: List[Any]) -> List[Any]:
     return [elem[0] for elem in groupby(the_list)]
 
 
-def flatten_list(list_of_list) -> List:
-    return list(chain.from_iterable(list_of_list))
+def flatten_list(list_of_lists: List[List[Any]]) -> List[Any]:
+    return list(chain.from_iterable(list_of_lists))
