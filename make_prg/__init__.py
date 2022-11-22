56ea4db84c5119316fd6c55a8285396011a2028f
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-11-22 19:02:44 +0000

    Updating version to 0.4.0

--- make_prg/__init__.py
+++ make_prg/__init__.py
@@ -4,6 +4,6 @@ from pkg_resources import get_distribution
 try:
     __version__ = get_distribution("make_prg").version
 except:
-    __version__ = "1.0.0"
+    __version__ = "0.4.0"
 
 __all__ = ["from_msa", "subcommands"]
