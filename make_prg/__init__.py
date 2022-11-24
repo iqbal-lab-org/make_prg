3bbfac77baa8fd92424fc4a9ffb01c6439948bd4
Michael Hall <michael@mbh.sh>
2022-11-24 13:25:53 +1000

    drop py3.7 support and update scikit-learn to 1.0

--- make_prg/__init__.py
+++ make_prg/__init__.py
@@ -1,15 +1,6 @@
 import sys
 
-"""
-Version has unique source in pyproject.toml.
-importlib fetches version from distribution metadata files
-(in dist-info or egg-info dirs).
-From Python 3.8, importlib_metadata is in standard library as importlib.metadata.
-"""
-if sys.version_info >= (3, 8):
-    from importlib import metadata
-else:
-    import importlib_metadata as metadata
+from importlib import metadata
 
 __version__ = metadata.version("make_prg")
 
