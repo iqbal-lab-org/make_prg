868305755179d6e91a1e9f6882c90ecac201a3b8
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-11-24 13:08:34 +0000

    Adding support to py3.7 back

--- make_prg/__init__.py
+++ make_prg/__init__.py
@@ -1,6 +1,15 @@
 import sys
 
-from importlib import metadata
+"""
+Version has unique source in pyproject.toml.
+importlib fetches version from distribution metadata files
+(in dist-info or egg-info dirs).
+From Python 3.8, importlib_metadata is in standard library as importlib.metadata.
+"""
+if sys.version_info >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
 
 __version__ = metadata.version("make_prg")
 
